aa471357334f8a8a31ebb40e796f98757f0cd323
sunkyu0384 <sunkyu0384@naver.com>
2024-09-03 18:34:14 +0900

    -

--- src/main/java/org/choongang/EurekaApplication.java
+++ src/main/java/org/choongang/EurekaApplication.java
@@ -1,4 +1,4 @@
-package org.hidog;
+package org.choongang;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/org/choongang/EurekaApplicationTests.java
+++ src/test/java/org/choongang/EurekaApplicationTests.java
@@ -1,4 +1,4 @@
-package org.hidog;
+package org.choongang;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
